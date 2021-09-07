7796b28ba0e7d712c3b796e35aefbf0eefebe87e
Lucas <46837214+foopsss@users.noreply.github.com>
2021-09-07 01:23:35 -0300

    Update Hosts.cmd

--- Hosts/Hosts.cmd
+++ Hosts/Hosts.cmd
@@ -11,8 +11,14 @@ attrib +R C:\Windows\System32\drivers\etc\hosts
 
 :: Credits
 :: •www.tutorialspoint.com - Code to make comments (Batch).
-:: •Kailash Dhondiyal/mytechnicalnotes.com - Code to turn off echo-ing (Batch).
+::  (https://www.tutorialspoint.com/batch_script/batch_script_comments.htm)
+:: •ServerFault - User "jftuga" - Code to turn off echo-ing (Batch).
+::  (https://serverfault.com/questions/187355/dont-show-batch-file-command-when-execute-it)
 :: •www.tutorialspoint.com - Code to change window title (Batch).
+::  (https://www.tutorialspoint.com/batch_script/batch_script_title.htm)
 :: •TechnoCraft/stackoverflow.com - Code to change window size (Batch).
+::  (https://stackoverflow.com/questions/8688846/resize-command-prompt-through-commands)
 :: •SRINI/www.windows-commandline.com - Code to change file attributes (Batch).
+::  (https://www.windows-commandline.com/set-read-only-attribute-file/)
 :: •Ryan Bemrose/stackoverflow.com - Code to invoke Powershell from CMD (Powershell).
+::  (https://stackoverflow.com/questions/41862786/how-can-i-download-a-file-from-the-internet-via-command-prompt)
