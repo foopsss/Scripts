65186be45b54ee21474074b7907638c2d66fa74d
Lucas <46837214+foopsss@users.noreply.github.com>
2021-12-03 11:39:47 -0300

    More tweaks
    Added the Powershell version of ZipGenerator and tweaked Calibri.sh.

--- Calibri/Calibri.sh
+++ Calibri/Calibri.sh
@@ -1,33 +1,48 @@
-# Defining script interpreter.
-
+# Declaro el intérprete a utilizar.
 #!/bin/bash
 
-# Process starts here
+# Defino una variable para almacenar una ruta.
+ruta=~/.local/share/fonts/calibri/
 
-echo "Installing fonts..."
+# Defino las funciones a utilizar.
+GBT() {
+# GBT es el acrónimo para Green Bold Text.
+# Esta función imprime texto blanco, sobre un fondo verde y en negritas.
+    echo -e "\e[1;42m$1\e[0m"
+}
+export -f GBT
 
-# Creating the directory and copying folder contents.
+RBT() {
+# RBT es el acrónimo para Red Bold Text.
+# Esta función imprime texto blanco, sobre un fondo rojo y en negritas.
+    echo -e "\e[1;41m$1\e[0m"
+}
+export -f RBT
 
-mkdir -p ~/.local/share/fonts/calibri
-find $(pwd)/ ! -name Calibri.sh -exec cp -t ~/.local/share/fonts/calibri/ {} +
+# Aquí comienza la operación.
+echo "Instalando fuentes..."
 
-# Making sure the process was successful.
+# Creo la carpeta y copio los contenidos.
+mkdir -p $ruta
+{
+    find "$(pwd)"/ ! -name Calibri.sh -exec cp -t $ruta {} +
+} 2>/dev/null
 
-cd ~/.local/share/fonts/calibri/
+# Me aseguro de que el proceso haya finalizado exitosamente.
+cd $ruta
 count=$(ls | wc -l)
 
 if [ ${count} -eq 6 ]
 then
- echo Done! Fonts installed in "~/.local/share/fonts/calibri/".
+ GBT "¡Hecho! Fuentes instaladas en '$ruta'."
 else
- echo The fonts have not been correctly installed. Please run the script again.
+ RBT "¡Error! Las fuentes no se instalaron correctamente. Por favor intente de nuevo."
 fi
 
-# Refreshing font cache.
-
+# Refresco la cache de las fuentes.
 fc-cache -v > /dev/null
 
-# Credits
+# Cŕeditos.
 << 'CREDITS'
 •tecadmin.net - Code to make comments.
  (https://tecadmin.net/tutorial/bash-comments)
